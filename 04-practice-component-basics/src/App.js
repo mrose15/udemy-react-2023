669ddb0f037488822a347ca83e89813e2bf4758e
Michele Cheow <ldnallen@gmail.com>
2023-09-14 09:21:27 -0700

    created component for Header

--- 04-practice-component-basics/src/App.js
+++ 04-practice-component-basics/src/App.js
@@ -1,8 +1,8 @@
-import keyConceptsImage from "./assets/images/key-concepts.png";
 import componentsImage from "./assets/images/components.png";
 import stateImage from "./assets/images/state.png";
 import eventsImage from "./assets/images/events.png";
 
+import Header from "./components/Header/Header";
 import Concepts from "./components/Concepts/Concepts";
 
 const concepts = [
@@ -26,17 +26,13 @@ const concepts = [
   },
 ];
 
-function App() {
+const App = () => {
   return (
     <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
+      <Header />
       <Concepts concepts={concepts}></Concepts>
     </div>
   );
-}
+};
 
 export default App;
