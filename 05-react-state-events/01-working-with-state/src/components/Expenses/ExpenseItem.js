6cffdddb35cd1616f2cfd48527e6a90f89a31468
Michele Cheow <ldnallen@gmail.com>
2023-09-15 22:46:38 -0700

    added useState hook and function for button click

--- 05-react-state-events/01-working-with-state/src/components/Expenses/ExpenseItem.js
+++ 05-react-state-events/01-working-with-state/src/components/Expenses/ExpenseItem.js
@@ -5,15 +5,21 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  // must be called directly inside component functions
+  // first is current, 2nd is updated value
+  // setTitle is a function that gets registered with useState
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log("Clicked!!!");
+    setTitle("Updated!"); //pass new value as arg, this is scheduled so the console.log will still output original value
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
