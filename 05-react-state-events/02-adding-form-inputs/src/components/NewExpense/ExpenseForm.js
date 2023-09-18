4ae42612918d1b567b8ed2f92fe6a7af147e4103
Michele Cheow <ldnallen@gmail.com>
2023-09-18 07:16:14 -0700

    set  up for adding form inputs lesson, added accessible labels

--- 05-react-state-events/02-adding-form-inputs/src/components/NewExpense/ExpenseForm.js
+++ 05-react-state-events/02-adding-form-inputs/src/components/NewExpense/ExpenseForm.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import './ExpenseForm.css';
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
   return (
     <form>
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
-          <label>Title</label>
-          <input type='text' />
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label for="title">Title</label>
+          <input id="title" type="text" />
         </div>
-        <div className='new-expense__control'>
-          <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+        <div className="new-expense__control">
+          <label for="amount">Amount</label>
+          <input id="amount" type="number" min="0.01" step="0.01" />
         </div>
-        <div className='new-expense__control'>
-          <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' />
+        <div className="new-expense__control">
+          <label for="date">Date</label>
+          <input id="date" type="date" min="2019-01-01" max="2022-12-31" />
         </div>
       </div>
-      <div className='new-expense__actions'>
-        <button type='submit'>Add Expense</button>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
