435200dbd53b4259257a075972afefc2961d3503
Michele Cheow <ldnallen@gmail.com>
2023-09-19 09:16:28 -0700

    added combined generic function as alt method of writing a function for multiple actions

--- 05-react-state-events/02-adding-form-inputs/src/components/NewExpense/ExpenseForm.js
+++ 05-react-state-events/02-adding-form-inputs/src/components/NewExpense/ExpenseForm.js
@@ -4,21 +4,62 @@ import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
   //initialize state with string
+  // using multiple states is more common, both approaches are fine
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
 
   // event arg occurs here as an event listener
   const titleChangeHandler = (e) => {
     setEnteredTitle(e.target.value);
+    // not good practice to update it like this, this depends on previous state snapshot to copy in the existing values and override one value with a new one
+    //whenever you update state and you depend on the previous state, you should use alt form, see below
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: e.target.value,
+    // });
+
+    // React schedules state update, it doesn't perform them instantly
+    //So if you schedule a lot of state updates at the same time, you could be depending on an outdated or incorrect state snapshot if you use the above approach
+    // Using the below will guarantee the state snapshot it gives you here in this inner function will always be the latest state snapshot, keeping all scheduled state updates in mind.
+    //*** Best way to track state ***///
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: e.target.value };
+    // });
   };
 
   const amountChangeHandler = (e) => {
     setEnteredAmount(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: e.target.value,
+    // });
   };
 
   const dateChangeHandler = (e) => {
     setEnteredDate(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: e.target.value,
+    // });
+  };
+
+  //shared changeHandler function as alternative
+  // react will not pass identifier and value on function call so you have to use a wrapper arrow function onChange
+  // onChange={(event) => inputChangeHandler('title', event.target.value)}
+  const inputChangeHandler = (identifier, value) => {
+    if (identifier === "title") {
+      setEnteredTitle(value);
+    } else if (identifier === "date") {
+      setEnteredDate(value);
+    } else {
+      setEnteredAmount(value);
+    }
   };
 
   return (
