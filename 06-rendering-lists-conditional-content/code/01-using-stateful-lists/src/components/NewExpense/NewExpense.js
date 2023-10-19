b661c9031e4f5159e44f1a3d35bad3e8efafdec9
Michele Cheow <ldnallen@gmail.com>
2023-10-19 11:59:46 -0700

    added state handler for expense form

--- 06-rendering-lists-conditional-content/code/01-using-stateful-lists/src/components/NewExpense/NewExpense.js
+++ 06-rendering-lists-conditional-content/code/01-using-stateful-lists/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
+  let formDisplay;
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
   };
 
+  if (showForm === true) {
+    formDisplay = (
+      <ExpenseForm
+        onSaveExpenseData={saveExpenseDataHandler}
+        hideFormHandler={() => {
+          setShowForm(false);
+        }}
+      />
+    );
+  }
+
   return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    <div className="new-expense">
+      <button
+        onClick={() => {
+          setShowForm(true);
+        }}
+      >
+        Add New Expense
+      </button>
+      {formDisplay}
     </div>
   );
 };
